fdc24a0a0d8a1bfb862563aac54361e6b07a2f8a
Long Jian <longjian11488@gmail.com>
2023-02-13 14:30:13 +0800

    Python Project

--- spider1/scrapytest.py
+++ spider1/scrapytest.py
@@ -1,4 +1,20 @@
 import scrapy
 class NewSpider(scrapy.Spider):
     name = "new_spider"
-    start_urls = ['http://bricksetcom/sets/year-2019']
+    start_urls = ['http://172.18.58.80/creative']
+
+    def parse(self, response):
+        xpath_selector = '//img'
+        for x in response.xpath(xpath_selector):
+            newsel = '@src'
+            yield {'Image Link': x.xpath(newsel).extract_first(), }
+
+# To recurse next page
+        page_selector = '.next a ::attr(href)'
+        next_page =  response.css(page_selector).extract_first()
+        if next_page:
+            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+
+
+
+
