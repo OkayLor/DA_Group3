69d0bb56f69c3eef8e8427f95199b7365b0d352a
Long Jian <longjian11488@gmail.com>
2023-02-20 14:50:39 +0800

    Python Project

--- TestMyProgram.py
+++ TestMyProgram.py
@@ -3,11 +3,11 @@ import unittest
 class TestMyProgram(unittest.TestCase):
 
     def test_upper(self):
-        self.assertEqual('http://172.18.58.80/creative'.upper(), 'http://172.18.58.80/creative')
+        self.assertEqual('HTTP://172.18.58.80/CREATIVE'.upper(), 'http://172.18.58.80/creative')
 
     def test_isupper(self):
         self.assertTrue('http://172.18.58.80/creative'.isupper())
-        self.assertFalse('http://172.18.58.80/Creative'.isupper())
+        self.assertFalse('HTTP://172.18.58.80/CREATIVE'.isupper())
 
 if __name__ == '__main__':
     unittest.main()
