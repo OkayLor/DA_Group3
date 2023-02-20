e58e1ffb65134363b05799a53abc913743b26879
Long Jian <longjian11488@gmail.com>
2023-02-20 15:40:14 +0800

    Python Project

--- TestMyProgram.py
+++ TestMyProgram.py
@@ -1,13 +1,19 @@
 import unittest
+import Browser
 
 class TestMyProgram(unittest.TestCase):
 
     def test_upper(self):
-        self.assertEqual('HTTP://172.18.58.80/CREATIVE'.upper(), 'http://172.18.58.80/creative')
+        self.assertEqual('http://172.18.58.80/creative'.upper(), 'HTTP://172.18.58.80/CREATIVE')
 
     def test_isupper(self):
-        self.assertTrue('http://172.18.58.80/creative'.isupper())
-        self.assertFalse('HTTP://172.18.58.80/CREATIVE'.isupper())
+        self.assertTrue('HTTP://172.18.58.80/CREATIVE'.isupper())
+        self.assertFalse('http://172.18.58.80/creative'.isupper())
+
+
+    def test_my_program(self):
+        result = my_program('http://172.18.58.80/creative', 'HTTP://172.18.58.80/CREATIVE') # Call your program function here
+        self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
